cb3b7df7904cf06dba7ae8415a6230357aa43c11
mfraggy25 <mfragiskatos.github@gmail.com>
2021-05-09 15:45:38 -0400

    first commit

---
+++ Template/_banner-adds.php
@@ -0,0 +1,7 @@
+<!---Banner Ads--->
+<section id="banner__adds">
+    <div class="container py-5 text-center">
+        <img src="./assets/banner_ad1.jpg" alt="banner1" class="img-fluid">
+        <img src="./assets/banner_ad2.jpg" alt="banner1" class="img-fluid">
+    </div>
+</section>
---
+++ Template/_banner-area.php
@@ -0,0 +1,14 @@
+<!---Owl Carousel--->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="./assets/banner1.jpg" alt="Banner1">
+        </div>
+        <div class="item">
+            <img src="./assets/banner2.jpg" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="./assets/banner3.jpg" alt="Banner3">
+        </div>
+    </div>
+</section>
